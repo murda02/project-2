e503ebe555d209eb66feea6f20912e28aaf486b5
Dave Murphy <dave.murphy@plusonerobotics.com>
2021-10-02 13:14:32 -0600

    added JawsDB back, fixed drink route name

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,14 +2,14 @@ const router = require ('express').Router();
 const {User} = require('../../models');
 
 //GET all users
-router.get('/', async (req, res) => {
-    try {
-        const userData = await User.findALl();
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const userData = await User.findALl();
+//         res.status(200).json(userData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 //GET a single user
 router.get('/:id', async (req, res) => {
