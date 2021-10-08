66735a31f3bfa6a39231eca510ff167262d3da1e
Dave Murphy <dave.murphy@plusonerobotics.com>
2021-10-07 20:56:00 -0600

    fixed login link

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,19 @@
 function generateLinks() {
     let linkEl = document.getElementById('top-nav-link');
-    console.log(linkEl);
-    console.log(document.location.pathname);
+    let linkEl2 = document.getElementById('top-nav-link-login');
     if (document.location.pathname === '/') {
-        console.log('If works');
         linkEl.href = '/user'
         linkEl.innerHTML = 'user page'
-    } else {
-        console.log('Else works');
+    } else if (document.location.pathname === '/user'){
         linkEl.href = '/'
         linkEl.innerHTML = 'home page'
+    } else {
+        linkEl2.href = '/'
+        linkEl2.innerHTML = 'home page'
     }
 }
 
 window.addEventListener('load', (event) => {
+    event.preventDefault();
     generateLinks();
 });
